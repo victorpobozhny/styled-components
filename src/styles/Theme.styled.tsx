2be97c35456e5ac438db2d9eb2248a1af769bb06
Victor <pobozhny94@gmail.com>
2024-03-05 17:40:57 +0300

    Contacts

--- src/styles/Theme.styled.tsx
+++ src/styles/Theme.styled.tsx
@@ -2,6 +2,7 @@ export const theme = {
     colors: {
         primary: 'gold',
         secondary: 'pink',
+        gradient: 'linear-gradient(90.00deg, rgb(0, 245, 160),rgb(0, 217, 245) 100%)',
         grey: {
             dark: '#555',
             bright: '#999'
