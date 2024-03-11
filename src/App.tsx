570bde9730511c043a4d46d0cd2c0a066e3fce95
Victor <pobozhny94@gmail.com>
2024-03-11 21:14:23 +0300

    Module 2 Contacts

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
             <Works/>
             <Skills/>
             <Contacts/>
-            {/*<Footer/>*/}
+            <Footer/>
         </Box>
     );
 }
--- src/styles/Theme.styled.tsx
+++ src/styles/Theme.styled.tsx
@@ -3,6 +3,8 @@ export const theme = {
         primaryBg: 'rgb(4, 31, 49)',
         secondaryBg: 'rgb(25,36,36)',
         accent: 'linear-gradient(90.00deg, rgb(0, 245, 160),rgb(0, 217, 245) 100%)',
+        inputBorder: 'rgb(189, 235, 234)',
+        placeholderColor: 'rgb(145, 155, 155)',
 
 
         font: {
